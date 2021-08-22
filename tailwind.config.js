1e0ef944f7b42de071c15b4a7ac1f7d707c48819
Oghenekaro Emmanuella <oghenekaro57@gmail.com>
2021-08-22 14:11:40 +0100

    done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    'index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderWidth:{
+        6: '6px'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
